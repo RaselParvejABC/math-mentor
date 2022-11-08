3b4429ba747423d75dcff81f56991157eb8d641d
Rasel Parvej <RaselParvejABC@gmail.com>
2022-11-08 14:12:56 +0600

    Banner Done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
     <div
-      className="p-12 text-center relative overflow-hidden bg-no-repeat bg-cover rounded-lg"
+      className="p-12 text-center relative overflow-hidden bg-no-repeat bg-cover rounded-lg rounded-t-none"
       style={{
         backgroundImage:
-          "url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp')",
+          "url('https://i.ibb.co/283rT7X/joanna-kosinska-1-CMo-Fs-Pfso-unsplash.jpg')",
         height: "400px",
       }}
     >
@@ -16,17 +17,23 @@ const Banner = () => {
       >
         <div className="flex justify-center items-center h-full">
           <div className="text-white">
-            <h2 className="font-semibold text-4xl mb-4">Heading</h2>
-            <h4 className="font-semibold text-xl mb-6">Subheading</h4>
-            <a
+            <h2 className="font-semibold text-4xl mb-4">
+              Mathematics can be scary, So is Love!
+            </h2>
+            <h4 className="text-lg mb-6">
+              Let's find that Love
+              <br /> between
+              <br /> You and Mathematics!
+            </h4>
+            <Link
               className="inline-block px-7 py-3 mb-1 border-2 border-gray-200 text-gray-200 font-medium text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-              href="#!"
+              to="/services"
               role="button"
               data-mdb-ripple="true"
               data-mdb-ripple-color="light"
             >
               See all Services
-            </a>
+            </Link>
           </div>
         </div>
       </div>
