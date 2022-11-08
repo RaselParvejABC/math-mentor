cee5ff695c1f92b4a6b3fd83aa120bf7dfa89493
Rasel Parvej <RaselParvejABC@gmail.com>
2022-11-08 15:13:16 +0600

    Testimonial on Home Done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import Testimonial from "../Testimonial/Testimonial";
 
 const Home = () => {
-  return <Banner />;
+  return (
+    <>
+      <Banner />
+      <Testimonial />
+    </>
+  );
 };
 
 export default Home;
--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -2,42 +2,58 @@ import React from "react";
 
 const Testimonial = () => {
   return (
-    <section class="mb-20 text-gray-700 px-4">
-      <div class="text-center max-w-3xl mx-auto">
-        <h3 class="text-3xl font-bold mb-6 text-gray-800">Testimonials</h3>
-        <p class="mb-6 pb-2 md:mb-12">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error
-          amet numquam iure provident voluptate esse quasi, veritatis totam
-          voluptas nostrum quisquam eum porro a pariatur veniam.
+    <section className="my-20 text-gray-700 px-4">
+      <div className="text-center max-w-3xl mx-auto">
+        <h3 className="text-3xl font-bold mb-6 text-gray-800">Testimonials</h3>
+        <p className="mb-6 pb-2 md:mb-12">
+          Let's hear from two of my special Clients!
         </p>
       </div>
 
-      <div class="grid md:grid-cols-2 gap-12 text-center">
-        <div class="mb-6 md:mb-0">
-          <div class="flex justify-center mb-6">
+      <div className="px-6 grid md:grid-cols-2 gap-12 text-center">
+        <div className="mb-6 md:mb-0">
+          <div className="flex justify-center mb-6">
             <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(22).jpg"
-              class="rounded-full shadow-lg w-24"
+              src="https://i.ibb.co/QCSRz8F/sherlock.jpg"
+              className="rounded-full shadow-lg w-24"
+              alt="Sherlock Holmes"
             />
           </div>
-          <p class="text-xl my-4 text-gray-500">
-            "Lorem ipsum dolor sit amet eos adipisci, consectetur adipisicing
-            elit sed ut perspiciatis unde omnis."
+          <p className="text-xl my-4 text-gray-500">
+            To defeat my greatest nemesis Professor James Moriarty, I had to
+            decode one of his Notebooks, the content of which was kind of
+            mathematically encrypted.
+            <br />
+            <br />
+            Without kind mentorship and help of Muhammad Rasel Parvej, the Math
+            Mentor here, it would be impossible, even for me.
+            <br />
+            <br />
+            And thanks, God! He is a not criminal!
           </p>
-          <p class="italic">- Anna Morian</p>
+          <p className="italic">- Sherlock Holmes</p>
         </div>
-        <div class="mb-0">
-          <div class="flex justify-center mb-6">
+        <div className="mb-0">
+          <div className="flex justify-center mb-6">
             <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(19).jpg"
-              class="rounded-full shadow-lg w-24"
+              src="https://i.ibb.co/sVWsnr6/batman.jpg"
+              className="rounded-full shadow-lg w-24"
+              alt="Batman"
             />
           </div>
-          <p class="text-xl my-4 text-gray-500">
-            "Neque cupiditate assumenda in maiores repudiandae mollitia
-            architecto elit sed adipiscing elit."
+          <p className="text-xl my-4 text-gray-500">
+            When the Riddler challenged to solve a Math Riddle to save Gotham,
+            the Joker and the Penguin laughed at me, meaning Math is not a thing
+            for Batman.
+            <br />
+            <br />
+            I couldn't prove them wrong on the spot. But eventually I did with
+            the help of Muhammad Rasel Parvej, our dear Math Mentor.
+            <br />
+            <br />
+            Justice was served!
           </p>
-          <p class="italic">- Teresa May</p>
+          <p className="italic">- Batman (Not Bruce Wayne)</p>
         </div>
       </div>
     </section>
