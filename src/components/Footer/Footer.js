e28e8ddcb05f85e39eb77e75660b656dcd54276c
Rasel Parvej <RaselParvejABC@gmail.com>
2022-11-08 14:47:51 +0600

    Footer Fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,20 +3,16 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="text-center text-white bg-blue-gray-800">
-      <div className="container p-6">
-        <div className="">
-          <p className="flex justify-center items-center">
-            <span className="mr-4">Join for Free.</span>
-            <button
-              type="button"
-              className="inline-block px-6 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-            >
-              <Link to="/register">Register</Link>
-            </button>
-          </p>
-        </div>
-      </div>
+    <footer className="text-white bg-blue-gray-800 p-6 text-center">
+      <p className="text-center">
+        Join for Free.
+        <button
+          type="button"
+          className="inline-block ml-3 px-6 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+        >
+          <Link to="/register">Register</Link>
+        </button>
+      </p>
 
       <div className="text-center p-4">
         &copy; {new Date().getFullYear()} —
