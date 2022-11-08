f70d5570ec64f2d47d14afe9a1a92daa491531e0
Rasel Parvej <RaselParvejABC@gmail.com>
2022-11-08 16:01:58 +0600

    Register Button on Footer Not shown when current route is register

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Footer = () => {
+  const location = useLocation();
+  const currentRoute = location.pathname;
   return (
-    <footer className="text-white bg-blue-gray-800 p-6 text-center">
+    <footer className="text-white bg-blue-gray-800 mt-6 p-6 text-center">
       <p className="text-center mt-4">
         Join for Free.
-        <button
-          type="button"
-          className="inline-block ml-3 px-6 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-        >
-          <Link to="/register">Register</Link>
-        </button>
+        {currentRoute === "/register" || (
+          <button
+            type="button"
+            className="inline-block ml-3 px-6 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+          >
+            <Link to="/register">Register</Link>
+          </button>
+        )}
       </p>
 
       <div className="text-center p-4">
