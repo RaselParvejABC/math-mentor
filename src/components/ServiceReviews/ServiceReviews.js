deb29c78f8473fc739bc581f3216463b1a26a445
Rasel Parvej <RaselParvejABC@gmail.com>
2022-11-09 22:31:52 +0600

    /service/:id Page Done

--- src/components/ServiceReviews/ServiceReviews.js
+++ src/components/ServiceReviews/ServiceReviews.js
@@ -1,9 +1,51 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Avatar,
+} from "@material-tailwind/react";
+import StarRatingComponent from "react-star-rating-component";
 
 const ServiceReviews = () => {
   const { reviews } = useLoaderData();
-  return <div>Service Reviews {reviews.length}</div>;
+  return (
+    <section className="mt-12 flex flex-col gap-20">
+      <Typography variant="h4" color="blue">
+        Reviews on this Service
+      </Typography>
+      {reviews.map((review) => {
+        return (
+          <Card cla>
+            <CardHeader>
+              <StarRatingComponent
+                className="text-center md:text-left text-2xl"
+                name="rating"
+                value={review.rating}
+                editing={false}
+                starCount={10}
+              />
+            </CardHeader>
+            <CardBody className="text-center">
+              <Typography>{review.reviewText}</Typography>
+            </CardBody>
+            <CardFooter
+              divider
+              className="flex items-center justify-between py-3"
+            >
+              <Typography variant="small" color="gray" className="flex gap-1">
+                {review.displayName}
+              </Typography>
+              <Avatar src={review.photoURL} alt="avatar" />
+            </CardFooter>
+          </Card>
+        );
+      })}
+    </section>
+  );
 };
 
 export default ServiceReviews;
